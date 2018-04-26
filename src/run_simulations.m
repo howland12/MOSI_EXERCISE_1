b67f46779f88e950df24294b38bb5e4bf92f244c
Unknown <j.hinum@student.tugraz.at>
2018-04-26 10:11:39 +0200

    create structure in simulate_semiconductor
    to handle function output of function simulate_semiconductor.m easier

--- src/run_simulations.m
+++ src/run_simulations.m
@@ -1,4 +1,4 @@
-close all;
+
 clearvars;
 
 run_GaAs = true;
@@ -17,17 +17,17 @@ run_Ge = true;
 %     run('main_Ge.m')
 % end
 
-[E_V, E_C, temperature, dopant_density, n_i, n, p, chemical_potential_i, chemical_potential, ionized_dopants, main_charge_carrier_number] = simulate_semiconductor('Si', 0.1, 'p-doped');
+[Si_p_doped] = simulate_semiconductor('Si', 0.1, 'p-doped');
 clf(figure(7))
 figure(7)
         hold on
 
-        plot(temperature,E_V * ones(size(temperature)),... 
+        plot(Si_p_doped.temperature,Si_p_doped.E_V * ones(size(Si_p_doped.temperature)),... 
              'LineWidth',1,'Color',[0 0 1],'DisplayName','Ge E_C');
-        plot(temperature,chemical_potential_i,'--','LineWidth',1,...
+        plot(Si_p_doped.temperature,Si_p_doped.chemical_potential_i,'--','LineWidth',1,...
              'Color',[1 0 0],'DisplayName','Ge E_F_intrinsic');
-        plot(temperature(find(chemical_potential < E_C)),...
-            chemical_potential(find(chemical_potential < E_C)),...
+        plot(Si_p_doped.temperature(find(Si_p_doped.chemical_potential < Si_p_doped.E_C)),...
+            Si_p_doped.chemical_potential(find(Si_p_doped.chemical_potential < Si_p_doped.E_C)),...
             'LineWidth',2,'Color',[0 1 0],'DisplayName','Ge \mu');
 
         title({'chemical potential vs temperature',' ',...
@@ -44,9 +44,9 @@ figure(8)
 
     hold on
 
-    plot(temperature, n_i/dopant_density,'.','LineWidth',1,'Color',...
+    plot(Si_p_doped.temperature, Si_p_doped.n_i/Si_p_doped.dopant_density,'.','LineWidth',1,'Color',...
          [1 0 0],'DisplayName','Ge');
-    plot(temperature, main_charge_carrier_number/dopant_density,'LineWidth',2,'Color',...
+    plot(Si_p_doped.temperature, Si_p_doped.main_charge_carrier_number/Si_p_doped.dopant_density,'LineWidth',2,'Color',...
          [1 0 0],'DisplayName','Ge');
 
     title({'electron density vs temperature',' ',...
@@ -62,13 +62,13 @@ figure(9)
 
 
     % correct for evalation errors
-    ionized_dopants(find(ionized_dopants == 1)) = 0;
+    Si_p_doped.ionized_dopants(find(Si_p_doped.ionized_dopants == 1)) = 0;
 
     hold on
     title({'number of ionized dopants vs temperature',' ',...
            'in Ge at N_D = 10^{21} m^3'}); 
 
-    plot(temperature, ionized_dopants,'LineWidth',2,'Color',[1 0 0],...
+    plot(Si_p_doped.temperature, Si_p_doped.ionized_dopants,'LineWidth',2,'Color',[1 0 0],...
          'DisplayName','Ge');
     legend('N_D^+/N_D', 'Location' ,'northeastoutside');
 
--- src/simulate_semiconductor.m
+++ src/simulate_semiconductor.m
@@ -1,4 +1,4 @@
-function [E_V, E_C, temperature, dopant_density, n_i, n, p, chemical_potential_i, chemical_potential, ionized_dopants, main_charge_carrier_number] = simulate_semiconductor( semiconductor, doping_energy, doping_type)
+function [struct_semiconductor] = simulate_semiconductor( semiconductor, doping_energy, doping_type)
 
 
     % -----------------------------------------------------------------------
@@ -159,68 +159,18 @@ function [E_V, E_C, temperature, dopant_density, n_i, n, p, chemical_potential_i
         
 
     end;
+         
+    
+    struct_semiconductor.E_V = E_V;
+    struct_semiconductor.E_C = E_C;
+    struct_semiconductor.temperature = temperature;
+    struct_semiconductor.dopant_density = dopant_density;
+    struct_semiconductor.n_i = n_i;
+    struct_semiconductor.chemical_potential_i = chemical_potential_i;
+    struct_semiconductor.chemical_potential = chemical_potential;
+    struct_semiconductor.ionized_dopants = ionized_dopants;
+    struct_semiconductor.main_charge_carrier_number = main_charge_carrier_number;
+    
 
     %%
-
-
- 
-
-% clf(figure(7))
-% figure(7)
-%         hold on
-% 
-%         plot(temperature,E_V * ones(size(temperature)),... 
-%              'LineWidth',1,'Color',[0 0 1],'DisplayName','Ge E_C');
-%         plot(temperature,chemical_potential_i,'--','LineWidth',1,...
-%              'Color',[1 0 0],'DisplayName','Ge E_F_intrinsic');
-%         plot(temperature(find(chemical_potential < E_C)),...
-%             chemical_potential(find(chemical_potential < E_C)),...
-%             'LineWidth',2,'Color',[0 1 0],'DisplayName','Ge \mu');
-% 
-%         title({'chemical potential vs temperature',' ',...
-%                'in Ge at N_D = 10^{21} m^3'});
-%         legend('E_C','\mu_i','\mu', 'Location' ,'northeastoutside');
-% 
-%         ylim([0 1.5]);
-%         xlabel('temperature / K');
-%         ylabel('energy / eV');
-
-
-%     clf(figure(8))    
-%     figure(8)
-% 
-%         hold on
-% 
-%         plot(temperature, n_i/dopant_density,'.','LineWidth',1,'Color',...
-%              [1 0 0],'DisplayName','Ge');
-%         plot(temperature, main_charge_carrier_number/dopant_density,'LineWidth',2,'Color',...
-%              [1 0 0],'DisplayName','Ge');
-% 
-%         title({'electron density vs temperature',' ',...
-%               'in Ge at N_D = 10^{21} m^3'}); 
-%         legend('n_i/N_D','n/N_D', 'Location' ,'northeastoutside');  
-% 
-%         ylim([0 2.0]);
-%         xlabel('temperature / K');
-%         ylabel('electron density / N_D');
-
-
-    clf(figure(9))    
-    figure(9)
-
-
-        % correct for evalation errors
-        ionized_dopants(find(ionized_dopants == 1)) = 0;
-
-        hold on
-        title({'number of ionized dopants vs temperature',' ',...
-               'in Ge at N_D = 10^{21} m^3'}); 
-
-        plot(temperature, ionized_dopants,'LineWidth',2,'Color',[1 0 0],...
-             'DisplayName','Ge');
-        legend('N_D^+/N_D', 'Location' ,'northeastoutside');
-
-        ylim([0 1.1]);
-        xlabel('temperature / K');
-        ylabel('density of ionized dopants/ N_D');
 
